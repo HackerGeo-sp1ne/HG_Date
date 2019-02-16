dc92618afcca01edd8e44ba4fb61c7517c43b6e8
HackerGeo <42976769+HackerGeo-sp1ne@users.noreply.github.com>
2019-02-16 15:03:55 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+--[[
+    FiveM Scripts
+    The Official HackerGeo Script 
+	WEBISTE: www.HackerGeo.com
+	GITHUB: GITHUB.com/HackerGeoTheBest
+	STEAM: SteamCommunity.com/id/HackerGeo1
+
+]]
+
+--------------------------------------------------------------------------------------------------------------------
+------------------------------------------- Official Scripts by HackerGeo ------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+
+
+
+client_scripts {
+	'time.lua'
+}
